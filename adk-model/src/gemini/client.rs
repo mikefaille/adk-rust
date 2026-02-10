c75ebbd569b09946b7741e5452e7bc328eecb2c6
Michael Faille <michael@faille.io>
2026-02-10 00:34:29 -0800

    feat: sync local updates for Gemini models and CLI

--- adk-model/src/gemini/client.rs
+++ adk-model/src/gemini/client.rs
@@ -209,6 +209,14 @@ impl GeminiModel {
                             id: None,
                         });
                     }
+                    adk_gemini::Part::CodeExecutionResult { code_execution_result } => {
+                        converted_parts.push(Part::CodeExecutionResult {
+                            code_execution_result: adk_core::CodeExecutionResultData {
+                                outcome: code_execution_result.outcome.clone(),
+                                output: code_execution_result.output.clone(),
+                            },
+                        });
+                    }
                     _ => {}
                 }
             }
@@ -326,6 +334,14 @@ impl GeminiModel {
                                     },
                                 });
                             }
+                            Part::CodeExecutionResult { code_execution_result } => {
+                                gemini_parts.push(adk_gemini::Part::CodeExecutionResult {
+                                    code_execution_result: adk_gemini::CodeExecutionResultData {
+                                        outcome: code_execution_result.outcome.clone(),
+                                        output: code_execution_result.output.clone(),
+                                    },
+                                });
+                            }
                             _ => {}
                         }
                     }
@@ -360,6 +376,14 @@ impl GeminiModel {
                                         thought_signature: None,
                                     },
                                     thought_signature: None,
+                                });
+                            }
+                            Part::CodeExecutionResult { code_execution_result } => {
+                                gemini_parts.push(adk_gemini::Part::CodeExecutionResult {
+                                    code_execution_result: adk_gemini::CodeExecutionResultData {
+                                        outcome: code_execution_result.outcome.clone(),
+                                        output: code_execution_result.output.clone(),
+                                    },
                                 });
                             }
                             _ => {}
